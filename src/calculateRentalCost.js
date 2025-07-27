da548bfe3281427c30d15c5da4477bb10886ac7d
Boris Mund <boris17527684@gmail.com>
2025-07-27 22:08:54 +0300

    Solve Task

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,31 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
-}
+  const PRICE_PER_DAY = 40;
+  const LONG_TERM = 7;
+  const BIG_DISCOUNT = 50;
+  const SHORT_TERM = 3;
+  const SMALL_DISCOUNT = 20;
 
+  let total = days * PRICE_PER_DAY;
+  if (days >= LONG_TERM) {
+    return total - BIG_DISCOUNT;
+  }
+  if (days >= SHORT_TERM) {
+    return total - SMALL_DISCOUNT;
+  }
+  return total;
+}
 module.exports = calculateRentalCost;
+
+// После тяжёлого квартала в офисе вы решаете отдохнуть в отпуске. Поэтому вы бронируете авиабилет для себя и своей семьи,
+// чтобы оставить весь этот бардак позади.
+
+// Вам понадобится арендованный автомобиль, чтобы передвигаться во время отпуска.
+// Менеджер проката автомобилей предлагает вам хорошие условия.
+
+// Каждый день аренды автомобиля стоит 40 долларов.
+// Если вы берёте машину на 7 или более дней, вы получаете скидку 50 долларов от общей суммы.
+// Или, если вы берёте машину на 3 или более дней, вы получаете скидку 20 долларов от общей суммы.
+
+// Реализуйте функцию calculateRentalCost, которая возвращает общую сумму аренды в зависимости от количества дней.
